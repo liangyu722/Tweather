8e31d057f1f96de3dea7978edaededd737801fcd
liangyu <liangyu722@gmail.com>
2020-01-28 21:35:08 +1100

    Add Weather Service

---
+++ app/src/main/java/com/twitter/challenge/data/network/WeatherService.kt
@@ -0,0 +1,18 @@
+package com.twitter.challenge.data.network
+
+import com.twitter.challenge.data.networking.response.WeatherResponse
+import kotlinx.coroutines.Deferred
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface WeatherService {
+
+    @GET("current.json")
+    fun currentWeatherAsync(): Deferred<WeatherResponse>
+
+    @GET("future_{day_in_advance}.json")
+    fun futureWeatherAsync(
+            @Path(value = "day_in_advance", encoded = true) dayInAdvance: Int
+    ): Deferred<WeatherResponse>
+
+}
