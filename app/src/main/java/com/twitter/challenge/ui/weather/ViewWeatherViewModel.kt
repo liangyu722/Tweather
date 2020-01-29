ba6fb37a7c0d063442010fe438866f1e138589c5
liangyu <liangyu722@gmail.com>
2020-01-30 09:56:09 +1100

    Add view model interface

---
+++ app/src/main/java/com/twitter/challenge/ui/weather/ViewWeatherViewModel.kt
@@ -0,0 +1,14 @@
+package com.twitter.challenge.ui.weather
+
+import androidx.lifecycle.LiveData
+import com.twitter.challenge.common.Event
+import com.twitter.challenge.model.TempStandDeviation
+import com.twitter.challenge.model.Weather
+
+interface ViewWeatherViewModel {
+
+    val loading: LiveData<Boolean>
+    val errorMessage: LiveData<Event<Int>>
+    val currentWeather: LiveData<Weather>
+    val temperatureStandDeviation: LiveData<TempStandDeviation>
+}
